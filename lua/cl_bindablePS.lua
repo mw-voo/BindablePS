8220cc91cbaeb4758603f990e6fe906757be9fbd
mw-voo <necro343@gmail.com>
2015-07-16 19:24:04 -0500

    Revision
    Conjoined PSext with the main sv_PS.

--- lua/cl_bindablePS.lua
+++ lua/cl_bindablePS.lua
@@ -2,17 +2,20 @@ local cd = false
 --bind x ps_weaponname
 function BindablePS(ply,stz,args,argz)
 	if !args[1] then return end
-	local tmp = args[1] or "nil"
+	print("Bind pressed")
+	print(argz)
 	net.Start("BindablePS_Request")
-	net.WriteString(tmp)
+	net.WriteString(argz)
 	net.SendToServer()
 end
 
-concommand.Add("ps_bind", BindablePS, _,_,_)
+concommand.Add("ps_bind", BindablePS, nil,nil,1024)
 
 net.Receive("BindablePS_Cooldown", function(len,ply)
 	local coolleft = net.ReadInt(10) 
 	chat.AddText("[BindablePS] Please wait " .. (math.Round(coolleft) or "{{<Error> (Net failed to send a proper number)}}") .. " more seconds before requesting this binded item.")
 
 end)
+
+
 
--- lua/sv_bindablePS.lua
+++ lua/sv_bindablePS.lua
@@ -1,9 +1,37 @@
+local meta = FindMetaTable( "Player" )
 
+function meta:setBindCooldown(t)
+	self.Cooldown = CurTime() + t
+end
+
+function meta:getBindCooldown()
+	local left = self.Cooldown or CurTime()
+	return left - CurTime()
+end
 
 net.Receive( "BindablePS_Request", function( len, ply )
+	print("net Recieved")
 	if !IsValid(ply) then return end
 	if not ply:PS_CanPerformAction() then return false end
-	local ps_item = net.ReadString()
+
+	print("Can Preform")
+	local item_net = net.ReadString()
+	print("net msg -> " .. item_net)
+	local ps_item = nil
+	--messy way of figuring out if the string is a item_id or a item  name
+	if !PS.Items[item_net] then
+		for _,v in pairs(PS.Items) do
+			if string.lower(v.Name) == string.lower(item_net) then
+				print("Found a match for set ".. v.Name)
+				ps_item = v
+				break
+			end
+		end
+	else
+		ps_item = item_net
+	end
+	if ps_item == nil then return end
+	print("Item not nil, continuing")
 
 	if ply:getBindCooldown() > 0 then
 		net.Start("BindablePS_Cooldown")
@@ -12,32 +40,31 @@ net.Receive( "BindablePS_Request", function( len, ply )
 		return 
 	end
 
-	--math.Round(ply:getTransferCooldown())
 	if ply:PS_HasItem(ps_item)  then	
-	--and !ply:PS_HasItemEquipped(ps_item)	
+		print("Preparing for clusterfuck")
 				local c_class = nil
-
-				if PS.Items[ps_item].WeaponClass ~= nil then
+				if ps_item.WeaponClass ~= nil then
 					c_class = true
-				elseif PS.Items[ps_item].Attachment ~= nil then
+				elseif ps_item.Attachment ~= nil then
 					c_class = false
 				else
 					c_class = nil
 				end
-
-
+				print("c_class == " .. c_class)
 				for k,v in pairs(PS.Items) do
-					local itm = v
-					if itm.WeaponClass ~= nil and itm.Equiped == true and c_class then
-						ply:PS_HolsterItem(itm)
-
-					elseif itm.Attachment ~= nil and itm.Equiped == true and !c_class then
-						ply:PS_HolsterItem(itm)
+					if (v.WeaponClass ~= nil or v.Attachment ~= nil  and v.Equiped == true and c_class or !c_class) then
+						print("Same class, holstering")
+						ply:PS_HolsterItem(v)
 					end
 				end
 				
-
+			print("Equiping")
 			ply:PS_EquipItem(ps_item)
+			if c_class then 
+				ply:SelectWeapon(ps_item.WWeaponClass)
+				print("Is weapon, selecting")
+			end
+			print("Done")
 			ply:setBindCooldown(GetConVar("ps_bind_cooldown"):GetInt())
 			return true	
 	else
