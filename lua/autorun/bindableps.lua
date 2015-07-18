9a424474f9d32005fdfc0e00bf3c8542536ef8e9
mw-voo <necro343@gmail.com>
2015-07-18 17:41:57 -0500

    Lowercased all file names

--- lua/autorun/bindableps.lua
+++ lua/autorun/bindableps.lua
@@ -5,8 +5,7 @@ if SERVER then
 
 	AddCSLuaFile()
 	AddCSLuaFile("cl_bindableps.lua")
-	include("sv_bindablePSext.lua")
-	include("sv_bindablePS.lua")
+	include("sv_bindableps.lua")
 	CreateConVar("ps_bind_cooldown", 15, 1408, "Changes the cooldown time to use a pointshop bind")
 	CreateConVar("ps_bind_allowedranks", "user", 1408, "The specific ranks that are allowed if reservedranks are enabled.")
 	CreateConVar("ps_bind_reservedranks", 0, 1408, "Enable/disable the ps_bind command to specific ranks")
@@ -15,5 +14,5 @@ if SERVER then
 	MsgAll("Registered BindablePS - By Voodoo(STEAM_0:1:28607710)")
 end
 if CLIENT then
-	include("cl_bindablePS.lua")
+	include("cl_bindableps.lua")
 end
